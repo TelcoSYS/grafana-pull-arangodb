6994987ac6522a0d5399ed1f250c7fe28708ccdd
cgc <cgc@cugli.life>
2018-05-06 23:03:23 -0300

    implement query for timeseries

--- main.js
+++ main.js
@@ -14,6 +14,14 @@ const router = createRouter();
 module.context.use(router);
 
 
+module.context.use(function (req, res, next) {
+   res.setHeader('Access-Control-Allow-Origin','*');
+   res.setHeader('Access-Control-Allow-Methods','POST');
+   res.setHeader('Access-Control-Allow-Headers','accept, content-type');
+   next();
+});   
+
+
 router.get('/', (req, res) => {
    const mt = req.context.manifest;
    res.send({ name: mt.name,
@@ -36,14 +44,67 @@ router.post('/search',(req, res) => {
 });
 
 
-router.post('/query',(req, res) => {
-   res.send({status:'Not implemented yet'});
-});
-
-
 router.post('/annotations',(req, res) => {
-   res.send({status:'Not implemented yet'});
+   const body = JSON.parse(req._raw.requestBody);
+   const reply = [];
+   console.info('anno',body);
+   const ql = 'FOR d IN @@coll FILTER (d.time >= @from AND d.time <= @to) OR (d.timeEnd >= @from AND d.timeEnd <= @to) RETURN d';
+   const antt = db._query(ql,{'@coll':module.context.collectionName('annotations')
+          ,from: new Date(body.range.from).getTime()
+	  ,to:   new Date(body.range.to).getTime() });
+   while (antt.hasNext()) {
+     let doc = antt.next();
+     let ant = { annotation: body.annotation }
+     ant.time = doc.time;
+     ant.text = doc.text;
+     ant.title = doc.title;
+     ant.tags = doc.tags;
+     if (doc.isRegion) {
+        ant.isRegion = true;
+	ant.timeEnd = doc.timeEnd;
+     }
+     reply.push(ant);
+     console.info('anot',ant);
+   }
+   res.send(reply);
 });
+
+
+
+router.post('/query',function (req, res) {
+   const body = JSON.parse(req._raw.requestBody);
+   const query = {}; const reply = [];
+   query.from = new Date(body.range.from).getTime();
+   query.to   = new Date(body.range.to).getTime();
+   query.interval = body.intervalMs;
+   query.targets = body.targets;
+
+   for (const t of body.targets) {
+     if(t.type == 'timeserie') 
+       reply.push(getTimeserieData(query,t));
+     else if(t.type == 'table')  	
+       reply.push(getTableData(query,t));
+   }
+   res.send(reply);
+});
+
+function getTimeserieData(q,t) {
+   const dat = db._query('FOR doc IN @@collection FILTER doc.time >= @from AND doc.time <= @to COLLECT bucket = FLOOR(doc.time/@interval)*@interval INTO values = doc.value RETURN  [AVERAGE(values),bucket]',
+               {'@collection': metricCollPrefix + t.target
+	         ,from:q.from,to:q.to,interval:q.interval}).toArray();
+   return { target:t.target, datapoints: dat };  
+}
+
+function getTableData(q,t) {
+   return { columns: [{text:"Time",type:"time"}
+                     ,{text:"Country",type:"string"}
+		     ,{text:"Number",type:"number"}],
+            rows:[[1234567,"SE",123]
+	         ,[1234567,"DE",231]
+		 ,[1234567,"US",321]], 
+            type:"table" };
+}
+
 
 
 
--- scripts/setup.js
+++ scripts/setup.js
@@ -1,26 +1,37 @@
 'use strict';
+
 const db = require('@arangodb').db;
-const documentCollections = [
-  "log",
-  "metrics",
-  "config"
-];
-const edgeCollections = [];
 
-for (const localName of documentCollections) {
-  const qualifiedName = module.context.collectionName(localName);
-  if (!db._collection(qualifiedName)) {
-    db._createDocumentCollection(qualifiedName);
-  } else if (module.context.isProduction) {
-    console.debug(`collection ${qualifiedName} already exists. Leaving it untouched.`)
-  }
-}
+if(!db._collection(module.context.collectionName("config"))) {
 
-for (const localName of edgeCollections) {
-  const qualifiedName = module.context.collectionName(localName);
-  if (!db._collection(qualifiedName)) {
-    db._createEdgeCollection(qualifiedName);
-  } else if (module.context.isProduction) {
-    console.debug(`collection ${qualifiedName} already exists. Leaving it untouched.`)
-  }
-}
+   //Run just one time
+   const documentCollections = [
+     "log",
+     "metrics",
+     "config"
+     ,"annotations"
+   ];
+
+   for (const localName of documentCollections) {
+      const qualifiedName = module.context.collectionName(localName);
+      if (!db._collection(qualifiedName)) {
+         db._createDocumentCollection(qualifiedName);
+      } else if (module.context.isProduction) {
+        console.debug(`collection ${qualifiedName} already exists. Leaving it untouched.`)
+      }
+   }
+
+   for (const c of ['metric_cpu','metric_mem','metric_temp']) 
+     if(!db._collection(c)){
+       db._createDocumentCollection(c);
+       db._query('LET now = DATE_NOW() FOR n IN 1..12000 LET tm = now - n*2*1000 INSERT { time: tm , extra:DATE_ISO8601(tm), value: RAND() } IN @@collection',{'@collection': c });
+   }
+   db._query('LET now = DATE_NOW() INSERT { time: now - 60*60*1000, title: "Incident #78493", timeEnd: now - 45*60*1000, isRegion:true, tags: ["docker","grafana"], text:"Service outage (Region annotation)" } INTO @@collection',{'@collection': module.context.collectionName('annotations')});
+
+   db._query('INSERT { time:DATE_NOW() - 20*60*1000, title: "Event #78534", tags: ["docker","arango"], text:"Service outage" } INTO @@collection',{'@collection': module.context.collectionName('annotations')});
+
+   
+   console.info(module.context.mount,'collections created');
+   module.exports = { msg:'collections created'};
+}   
+
